e1c6b406be608879a3b013d34fc08a155288bb84
Migsar Navarro <migsar.navarro@gmail.com>
2016-08-16 07:23:15 -0500

    Initial release

---
+++ browser/js/vendor.js
@@ -0,0 +1,10 @@
+(function (window) {
+  "use strict";
+
+  window.jQuery = require('jquery');
+  window.Tether = require('tether');
+  window.THREE = require('three');
+  window.angular = require('angular');
+  require('../../node_modules/bootstrap/dist/js/bootstrap.min');
+
+})(window);
